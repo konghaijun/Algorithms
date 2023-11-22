479b1b7733761d4230c188d6b9ab8ffd0b04048e
for shell <fromShell@paas.com>
2023-11-22 19:53:01 +0800

    tow

--- src/main/java/com/my/practice/dd.java
+++ src/main/java/com/my/practice/dd.java
@@ -2,4 +2,6 @@ package com.my.practice;
 
 
 public class dd {
+
+  
 }
--- src/main/java/com/my/practice/hello.java
+++ src/main/java/com/my/practice/hello.java
@@ -1,13 +1,79 @@
 package com.my.practice;
- public class hello {
+
+
+public class hello {
 
   public static void main(String[] args) {
-int[] nums=new int[]{3,2,2,3,3};
-    int val=3;
-    System.out.print(removeElement(nums,val));
-  
+int[] nums=new int[]{2,3,1,2,4,3};
+    //int val=3;
+    //System.out.print(removeElement(nums,val));
+ // System.out.print(maxSubArray(nums));
+  System.out.print(minSubArrayLen(7,nums));
+  }
+
+
+public static int minSubArrayLen(int target, int[] nums) {
+
+  int left=0;
+  int right=0;
+  int result=Integer.MAX_VALUE;
+  int sum=nums[0];
+  //int start=-1;
+  for(int i=1;i<nums.length;i++){
+    if(sum<target){
+      right++;
+      sum=sum+nums[right];
+    }else {
+      result=Math.min(result,right-left+1);
+      sum=sum-nums[left];
+      left++;
+    }
   }
   
+  return result;
+    }
+
+
+
+   
+
+
+//最大子序列和
+    public static   int maxSubArray(int[] nums) {
+      //int[] dp=new int[nums.length];
+      int left=nums[0];
+      int result=nums[0]; 
+      for(int i=1;i<nums.length;i++){
+      left=Math.max(left+nums[i],nums[i]);
+      result=Math.max(left,result);
+        }
+      return result;
+    }
+   
+   
+//有序平方数组
+public static int[] sortedSquares(int[] nums) {
+   int left=0; int right=nums.length-1;
+   int[] num=new int[nums.length];
+   int i=nums.length-1;
+  while(left<=right){
+    if(nums[left]*nums[left]>=nums[right]*nums[right]){
+      num[i]=nums[left]*nums[left];
+      i--;
+      left++;
+    }else{
+      num[i]=nums[right]*nums[right];
+      i--;
+      right--;
+    }
+  }
+  
+  return num;
+    }
+
+
+   
+   //移除元素
  public static int removeElement(int[] nums, int val) {
        int left =0;
        int right=0;
@@ -26,7 +92,7 @@ int[] nums=new int[]{3,2,2,3,3};
 
 
 
-  
+  //二分查找
   public static int search(int[] nums,int targer){
     int left =0;
     int right=nums.length-1;
